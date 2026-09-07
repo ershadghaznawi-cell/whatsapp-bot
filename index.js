4917af820fb3d4427503a074ce6f5416d58e1498
ershadghaznawi-cell <ershadghaznawi@gmail.com>
2026-09-07 14:00:24 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const crypto = require('crypto');
+global.crypto = crypto;
+
 const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require('@whiskeysockets/baileys');
 const pino = require('pino');
 
@@ -12,7 +14,12 @@ async function connectToWhatsApp() {
     });
 
     sock.ev.on('connection.update', (update) => {
-        const { connection, lastDisconnect } = update;
+        const { connection, lastDisconnect, qr } = update;
+        
+        if (qr) {
+            console.log('QR CODE RECEIVED:', qr);
+        }
+
         if (connection === 'close') {
             const shouldReconnect = lastDisconnect?.error?.output?.statusCode !== DisconnectReason.loggedOut;
             console.log('اړیکه وشلول شوه، د بیا نښلیدو په حال کې ده...', shouldReconnect);
